73b3868bb2e1ea7783c00d269f2e17ed7c896be1
Advik <adviksingh6632@gmail.com>
2023-12-11 01:30:51 +0530

    solve the problem

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+)
+
 type Grade string
 
 const (
@@ -20,18 +26,86 @@ type studentStat struct {
 	grade      Grade
 }
 
-func parseCSV(filePath string) []student {
-	return nil
+func parseInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
 }
 
-func calculateGrade(students []student) []studentStat {
-	return nil
+func parseCSV(filePath string) []student {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	data, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	var result []student
+	for i, row := range data {
+		if i == 0 {
+			continue // skip header row
+		}
+		result = append(result, student{
+			firstName:  row[0],
+			lastName:   row[1],
+			university: row[2],
+			test1Score: parseInt(row[3]),
+			test2Score: parseInt(row[4]),
+			test3Score: parseInt(row[5]),
+			test4Score: parseInt(row[6]),
+		})
+	}
+	return result
+}
+
+func calculateGrade(students []student) (result []studentStat) {
+	for _, s := range students {
+		score := float32(s.test1Score+s.test2Score+s.test3Score+s.test4Score) / 4
+		var grade Grade
+		switch {
+		case score >= 70:
+			grade = A
+		case score >= 50:
+			grade = B
+		case score >= 35:
+			grade = C
+		default:
+			grade = F
+		}
+
+		result = append(result, studentStat{
+			student:    s,
+			finalScore: score,
+			grade:      grade,
+		})
+	}
+	return
 }
 
 func findOverallTopper(gradedStudents []studentStat) studentStat {
-	return studentStat{}
+	topper := gradedStudents[0]
+	for _, s := range gradedStudents {
+		if s.finalScore > topper.finalScore {
+			topper = s
+		}
+	}
+	return topper
 }
 
 func findTopperPerUniversity(gs []studentStat) map[string]studentStat {
-	return nil
+	universityToppers := make(map[string]studentStat)
+	for _, s := range gs {
+		topper, present := universityToppers[s.student.university]
+		if !present || s.finalScore > topper.finalScore {
+			universityToppers[s.student.university] = s
+		}
+	}
+	return universityToppers
 }
